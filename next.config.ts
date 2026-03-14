a2a7fd3b48a9d2ab0eb8fb587f75d4d0de8c7258
Arfaa Mumtaz <mumtazm@mymacewan.ca>
2026-03-14 15:44:37 -0600

    Update config and package files

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://teyrckdarkmenzatviqt.supabase.co",
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY: "sb_publishable_mHynPxKhpz6fEf6hWrAUvA_qxVHVmEq",
+  },
+};
+
+export default nextConfig;
