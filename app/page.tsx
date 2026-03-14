692f6f1aced00fa714f77a605f5498a164eaf875
Arfaa Mumtaz <mumtazm@mymacewan.ca>
2026-03-14 15:46:10 -0600

    adjust to hold leadform content

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LeadForm from '@/components/LeadForm'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <LeadForm />
+    </main>
+  )
+}
