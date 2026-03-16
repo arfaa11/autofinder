2dc2d3e9dbe642ec4c21cf78b4fac25e4c070856
Arfaa Mumtaz <mumtazm@mymacewan.ca>
2026-03-15 23:43:16 -0600

    temp resend var

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: "https://teyrckdarkmenzatviqt.supabase.co",
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY: "sb_publishable_mHynPxKhpz6fEf6hWrAUvA_qxVHVmEq",
+    RESEND_API_KEY: "re_3sQRQVcQ_CL5ykDcLpVRGAGoeAiky8vC2"
   },
 };
 
